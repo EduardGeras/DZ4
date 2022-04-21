389e37ace9f07c882cd86e28ff969a7ec82d2eaf
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-04-21 19:58:22 +0500

    домашка 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задание 2
+        int clientDeviceYear = 2012;
+        int clientOs = 1;
+        if (clientOs == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+        } else if (clientOs != 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else {
+            System.out.println("Установите облегчённую версию приложения для IOS по ссылке");
+        }
+
+        // Задание 3
+        int year = 2021;
+        boolean leapYear = (year % 4 == 0) && (year % 100 != 0);
+        if (year % 400 == 0 || leapYear) {
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+
+        // Задание 4
+        int deliveryDistance = 95;
+        int day = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Понадобится дней: " + day);
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Понадобится дней: " + (day + 1));
+        } else if (deliveryDistance <= 100) {
+            System.out.println("Понадобится дней: " + (day + 2));
+        } else {
+            System.out.println("Вы слишком далеко от нас))");
+        }
+
+        // Задание 5
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
 }
